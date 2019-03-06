6cbb76ff551426135d314756aded0d7fdba2b89c
Yoni Sisso <yonis9@walla.com>
2019-03-06 19:09:51 +0200

    fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-encircled-88298',
     user : 'postgres',
     password : 'dbpass',
     database : 'smartbrain'
